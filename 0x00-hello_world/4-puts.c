e3246f71241101004ec7b5dd25d307f5153aa06e
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-16 13:09:58 -0800

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+
+
 /**
  * main
- * this program write "Programming is like building a multilingual puzzle"
+ * this program write ("Programming is like building a multilingual puzzle)
+ * followed by a new line
  * Return 0.
  */
 
+
 int main (void)
-{
+{	
 	puts ("\"Programming is like building a multilingual puzzle");
 	
 	return (0);
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main
+ * Write a C program that prints exactly with proper grammar, but the outcome is a piece of art
+ * followed by a new line
+ * Return
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	
+	return(0);
+
+}
+
+
