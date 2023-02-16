e326dd5b7e2ae207a2a861cb4121bb6007994e95
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-16 11:38:50 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
+
 /**
  * this program write "Programming is like building a multilingual puzzle"
  */
+
 int main (void)
 {
-        echo "Programming is like building a multilingual puzzle\n";
-	return 0;
+        puts ("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
 }
