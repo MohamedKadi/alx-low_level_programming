45e84ca572d617c25178918fff93a1ca1f8d07e5
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-16 11:23:59 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stio.h>
+/**
+ * this program write "Programming is like building a multilingual puzzle"
+ */
+int main (void)
+{
+	printf("Programming is like building a multilingual puzzle");
+	return 0;
+}
