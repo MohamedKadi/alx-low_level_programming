dccc808b8f286772998989442ea3b0aa447a97d1
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-17 13:31:56 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	while
+		(a <= 'a') {
+			if ((a != 'q' && a != 'e') && a <= 'z')
+				putchar (a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
