85f3965fc78e493a502244ff9b2337a3d7d9859a
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-17 14:59:08 -0800

    8 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	int b;
+
+	a = 'a';
+	while
+		(b < 10) {
+			putchar(b + '0');
+			b++;
+		}
+	while
+		(a < 'g') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
