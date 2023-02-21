ce50e5e5390e1d37e6d2f37197090162ba25720d
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-21 11:53:17 -0800

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * int print_last_digit - entry point
+ *@n: the last digit
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return(0);
+}
