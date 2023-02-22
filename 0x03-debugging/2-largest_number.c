5e3878b764a1180a4486a836882fa5ce2872b52f
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-22 01:21:49 -0800

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,11 +14,11 @@ int largest_number(int a, int b, int c)
 
 int largest;
 
-if (a >= b && b >= c)
+if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b >= a && a >= c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
@@ -28,4 +28,25 @@ largest = c;
 }
 
 return (largest);
+}
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+{
+int a, b, c;
+int largest;
+
+a = 972;
+b = -98;
+c = 0;
+
+largest = largest_number(a, b, c);
+
+printf("%d is the largest number\n", largest);
+
+return (0);
 }
