e583068ca5dcf0f99e35f3e41703a2a9ecfe0ef1
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-23 09:50:29 -0800

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  more_numbers - entry point
+ *  Return: 0
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			_putchar(y + '0');
+		}
+	}
+	_putchar('\n');
+}
