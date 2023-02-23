918350cc84431d505338056cfe4a549df12304a8
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-23 09:34:08 -0800

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x + '0');
+		}
+	}
+}
