3cd4153d1e041c2ffea8b8224fd10f98e6ab2e8c
MohamedKadi <gamesmohamed14@gmail.com>
2023-02-27 09:36:42 -0800

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,15 +4,16 @@
 /**
  * swap_int - entry point
  * @a: first num
- * @sec num
- * Return:0
+ * @b: sec num
+ * Return: 0
  */
 
 void swap_int(int *a, int *b)
+
 {
 	int temp = *a;
 
 	*a = *b;
 	
-	*b = *temp;
+	*b = temp;
 }
